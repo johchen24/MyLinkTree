bb8d9721e0c48722927c44e337e6d2414ca11fdf
Johnnie <johchen24@gmail.com>
2025-06-25 01:05:56 -0400

    fixed main form

--- src/components/forms/MainForm.js
+++ src/components/forms/MainForm.js
@@ -1,14 +1,25 @@
 "use client";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 export default function MainForm() {
-    function handleSubmit(e){
+    const [username, setUsername] = useState("");
 
+    async function handleSubmit(e){
+        e.preventDefault();
+        if (username.length > 0){
+            await signIn('google', {callbackUrl: '/account?username='+username})
+        }
     }
-    
+
     return(
         <form onSubmit={handleSubmit}className="inline-flex items-center shadow-lg shadow-grey-500/20">
             <span className="bg-white py-4 pl-4">mylinktree.to/</span>
-            <input type="text" className="py-4 bg-white" placeholder="username"/>
+            <input 
+                type="text" className="py-4 bg-white" placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
             <button type="submit" className="bg-blue-500 text-white py-4 px-6">
                 Create Now
             </button>
