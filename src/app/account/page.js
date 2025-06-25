a7b96c5e9b7fea3a6732d5585e874f17de88e5eb
Johnnie <johchen24@gmail.com>
2025-06-25 17:20:25 -0400

    server action setup

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { FaRegHandPointRight } from "react-icons/fa";
+import claimUsername from "@/actions/claimUsername";
 
 export default async function Account({searchParams}) {
     const session = await getServerSession(authOptions);
@@ -12,7 +13,7 @@ export default async function Account({searchParams}) {
     }
     return(
         <div>
-            <form>
+            <form action={claimUsername}>
                 <h1 className="text-4xl font-bold text-center mb-2">
                     Grab your username
                 </h1>
@@ -22,7 +23,7 @@ export default async function Account({searchParams}) {
                 <div className="max-w-xs mx-auto">
                     <input 
                         className="block p-2 bg-white border-2 border-gray-300 rounded-md w-full mb-2 text-center" 
-                        type="text" placeholder="Username" defaultValue={username}/>
+                        type="text" name="Username" placeholder="Username" defaultValue={username}/>
                     <button
                         type="submit" 
                         className="bg-emerald-700 text-white px-4 py-2 rounded-md block w-full flex items-center justify-center gap-2"
