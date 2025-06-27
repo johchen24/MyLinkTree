6a7cb90217424b6560980b1fd35821104262540b
Johnnie <johchen24@gmail.com>
2025-06-27 16:18:49 -0400

    add page schema

--- src/actions/claimUsername.js
+++ src/actions/claimUsername.js
@@ -1,5 +1,11 @@
 'use server';
 
+import { Page } from "@/models/pageSchema";
+import mongoose from "mongoose";
+
 export default async function claimUsername(formData) {
     const username = formData.get('Username');
+    mongoose.connect(process.env.MONGO_URI);
+    const pageDoc = await Page.create({ uri : username}); // must return plain object to client component
+    return pageDoc.toJson()
 }
--- src/models/pageSchema.js
+++ src/models/pageSchema.js
@@ -4,4 +4,4 @@ const PageSchema = new Schema({
     uri: { type: String, required: true, min: 1, unique: true},
 }, { timestamps: true });
 
-const Page = models?.Page || model('Page', PageSchema);
+export const Page = models?.Page || model('Page', PageSchema);
