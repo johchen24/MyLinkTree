6debd15f1aa71802cc3e5a557f675f3f2b904960
Johnnie <johchen24@gmail.com>
2025-07-12 13:31:29 -0400

    moved sidebar to template.js

--- src/components/buttons/LogoutButton.js
+++ src/components/buttons/LogoutButton.js
@@ -2,11 +2,11 @@
 import { signOut } from "next-auth/react";
 import { MdOutlineLogout } from "react-icons/md";
 
-export default function LogoutButton() {
+export default function LogoutButton({className = "flex items-center gap-1 border border-emerald-900/80 p-2 px-4 shadow rounded-md"}) {
     return (
-        <button onClick={() => signOut()} className="flex items-center gap-1 border border-slate-200 p-2 px-4 shadow rounded-md">
+        <button onClick={() => signOut()} className={className}>
+            <MdOutlineLogout className="w-6 h-6"/>
             <span>Sign out</span>
-            <MdOutlineLogout className="" />
         </button>
     )
 }
---
+++ src/components/layout/AppSidebar.js
@@ -0,0 +1,30 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { MdOutlineSwitchAccount } from "react-icons/md";
+import { FaChartLine } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+import LogoutButton from "@/components/buttons/LogoutButton";
+
+export default function AppSidebar() {
+    const path = usePathname();
+    console.log(path);
+
+    return (
+        <nav className="inline-flex mx-auto flex-col text-center mt-12 gap-4 text-emerald-950">
+            <Link href="/account" className={"flex gap-4 p-2 rounded-lg " + (path === '/account' ? 'bg-emerald-900/10 font-bold' : '')}>
+                <MdOutlineSwitchAccount className="w-6 h-6"/>
+                <span>My Page</span>
+            </Link>
+            <Link href="/analytics" className={"flex gap-4 p-2 rounded-lg " + (path === '/analytics' ? 'bg-emerald-900/10 font-bold' : '')}>
+                <FaChartLine className="w-6 h-6"/>
+                <span>Analytics</span>
+            </Link>
+            <LogoutButton className={'flex gap-4 p-2 rounded-lg'}/>
+            <Link href="/" className="flex gap-4 p-2 rounded-lg border-t border-emerald-900/60 pt-4 mt-4">
+                <FaHome className="w-6 h-6"/>
+                <span>Back to Home</span>
+            </Link>
+        </nav>
+    );
+}
