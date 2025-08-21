344284fcab240833235e77b1a430961e37126d0b
Johnnie <johchen24@gmail.com>
2025-08-21 00:56:41 -0400

    setup s3

--- src/components/forms/PageSettingsForm.js
+++ src/components/forms/PageSettingsForm.js
@@ -18,31 +18,52 @@ export default function PageSettingsForm({page, user}) {
         }
     }
 
+    function handleFileChange(e){
+        const file = e.target.files?.[0];
+        if (file){
+            const data = new FormData;
+            data.set('file', file);
+            fetch('/api/upload', {
+                method: 'POST',
+                body: data
+            }).then(res => res.json())
+        }
+    }
+
     return (
         <div className="-m-4">
             <form action={saveBaseSettings}>
                 <div className="bg-gray-300 h-72 flex justify-center items-center flex-col mb-8" style={{backgroundColor: bgType === 'color' ? bgColor : 'transparent'}}>
                     <div className="bg-white rounded-lg p-3 shadow-lg">
                         <div className="radio-togglers mb-4"> {/* This went into radio togglers*/}
-                            <label>
-                                <input type="radio" name="bgType" value="color" defaultChecked={page.bgType === 'color'}/>
+                            <label className="input-label">
+                                <input type="radio" name="bgType" value="color" defaultChecked={bgType === 'color'} onClick={() => setBgType('color')}/>
                                 <div>
                                     <MdColorLens />
                                     <span>Color</span>
                                 </div>
                             </label>
-                            <label>
-                                <input type="radio" name="bgType" value="image" defaultChecked={page.bgType === 'image'}/>
+                            <label className="input-label">
+                                <input type="radio" name="bgType" value="image" defaultChecked={bgType === 'image'} onClick={() => setBgType('image')}/>
                                 <div>
                                     <FaImages />
                                     <span>Image</span>
                                 </div>
                             </label>
-                        </div>
+                        </div> {/* radio togglers*/}
                         {bgType === 'color' && (
                             <div className="flex items-center gap-2 justify-center">
-                                <span>Background Color</span>
-                                <input type="color" name="bgColor" defaultValue={page.bgColor}/>         
+                                <span>Change Background Color</span>
+                                <input type="color" name="bgColor" className="rounded-full" onChange={(e) => setBgColor(e.target.value)} defaultValue={page.bgColor}/>         
+                            </div>
+                        )}
+                        {bgType === 'image' && (
+                            <div className="flex justify-center">
+                                <label className="border border-gray-300/50 rounded-lg p-2 px-4 shadow-md">
+                                    <span>Change Background Image</span>
+                                    <input type="file" name="bgImage" className="hidden" onChange={handleFileChange}/>
+                                </label>
+
                             </div>
                         )}
                     </div>
@@ -58,11 +79,11 @@ export default function PageSettingsForm({page, user}) {
                     />
                 </div>
                 <div className="p-4 styled">
-                    <label htmlFor="nameIn">Display Name</label>
+                    <label htmlFor="nameIn" className="input-label">Display Name</label>
                     <input type="text" name="displayName" defaultValue={page.displayName} id="nameIn" placeholder="John Doe"/>
-                    <label htmlFor="locationIn">Location</label>
+                    <label htmlFor="locationIn" className="input-label">Location</label>
                     <input type="text" name="location" defaultValue={page.location} id="locationIn" placeholder="New York, NY"/>
-                    <label htmlFor="bioIn">Bio</label>
+                    <label htmlFor="bioIn" className="input-label">Bio</label>
                     <textarea name="bio" defaultValue={page.bio} id="bioIn" placeholder="Your Bio..."/>
                     <div className="max-w-md mx-auto mt-4">
                         <SubmitButton>
